5207896a537c7521bda361efbfebeca01ccb3c06
barslmn <bar@barsdeb.barsdeb>
2018-10-04 16:17:49 +0300

    modified:   pyfq/core/qcfq.py 	new file:   pyfq/examples/SP1.fq.gz 	deleted:    pyfq/utils/catfq.py 	modified:   pyfq/utils/fastq.py

--- pyfq/core/qcfq.py
+++ pyfq/core/qcfq.py
@@ -1,58 +1,33 @@
 import matplotlib.pyplot as plt
 
+import os
 
-class FastqQualityCheck():
+# Comment out this before building
+#from pyfq.utils.fastq import Fastq
+# Remove this part after dev
+import sys
+sys.path.append(os.path.abspath('..'))
+from utils.fastq import Fastq
+
+
+class FastqQualityCheck(Fastq):
 
     """Docstring for FastqQualityCheck. """
 
-    def __init__(self, fastqfile):
-        self.fastqfile = fastqfile
-
-    def __str__(self):
-        s = ''
-        for read in self.readfastqfile:
-            s += read[1]
-        return s
-
-    def __len__(self):
-        readlengths = [str(len(read[1])) for read in self.readfastqfile]
-        return set(readlengths)
-
-    def chunks(self, l, n):
-        '''Yield successive n-size chunks from l.'''
-        for i in range(0, len(l), n):
-            yield l[i:i + n]
-
-    @property
-    def readfastqfile(self):
-        with open(self.fastqfile, 'r') as f:
-            reads = f.read()
-            f.close()
-        groups = reads.split('\n')[:-1]
-        read_groups = [chunk for chunk in self.chunks(groups, 4)]
-        return read_groups
+    def __init__(self, fastq_file):
+        super().__init__(fastq_file)
 
     def translatephredscores(self):
-        self.p_errors = []
-        for read in self.readfastqfile:
-            self.p_errors.append([10**-((ord(phredscore) - 33) / 10)
-                                  for phredscore in read[3]])
-        return self.p_errors
+        new_fastq = {}
+        for identifier, read, phred_scores in self:
+            new_fastq[identifier] = [read, [10**-((ord(phred_score) - 33) / 10) for phred_score in phred_scores]]
+        self.fastq = new_fastq
 
-    def convert2fasta(self):
-        fastareads = [reads[:2] for reads in self.readfastqfile]
-        fastareads = '\n'.join(
-            [reads[0].replace('@', '>', 1) + '\n' + reads[1] for reads in fastareads])
-        return fastareads
-
-    def writefasta(self, fastafile, fastareads):
-        with open(fastafile, 'w') as f:
-            f.write(fastareads)
-            f.close()
-
-    def drawboxplot(self, p_errors):
-        plt.boxplot(list(map(list, zip(*p_errors))), 0, '')
+    def drawboxplot(self):
+        plt.boxplot(list(map(list, zip(*[read_record[2] for read_record in self]))), 0, '')
         plt.show()
+
+
 
 def main():
     """TODO: Docstring for main.
--- pyfq/utils/fastq.py
+++ pyfq/utils/fastq.py
@@ -1,35 +1,71 @@
+import os
+import sys
+import gzip
 
-class Fastq(object):
+
+class Fastq():
 
     def __init__(self, fastq_file):
         self.fastq_file = fastq_file
+        self.read_fastq()
 
     def __str__(self):
-        return ''
+        return 'Fastq object for {}'.format(os.path.basename(self.fastq_file))
 
     def __repr__(self):
-        return ''
+        return 'Fastq object for {}'.format(os.path.basename(self.fastq_file))
 
     def __len__(self):
-        '''Returns number of reads
-        '''
-        return  len(self.read_records)
+        '''Returns number of reads'''
+        return len(self.fastq)
+
+    def __iter__(self):
+        for k, v in self.fastq.items():
+            yield k, v[0], v[1]
 
     def chunks(self, l, n):
         '''Yield successive n-size chunks from l.'''
         for i in range(0, len(l), n):
             yield l[i:i + n]
 
-    def read_fastq(self):
-        with open(self.fastq_file, 'r') as f:
-            contents = f.read()
-            f.close()
-        all_reads = contents.split('\n')[:-1]
-        self.read_records = {chunk[0]:[chunk[1], chunk[3]] for chunk in self.chunks(all_reads, 4)}
-        yield self.read_records
+    @property
+    def fastq(self):
+        return self.read_records
 
-a = ParseFastq('/home/bar/Workbench/pybench/pyfqc/SP1.fq')
-a.read_fastq()
-len(a)
-for i in a.read_fastq:
-    print(i)
+    @fastq.setter
+    def fastq(self, new_records):
+        self.read_records = new_records
+        
+
+    def read_fastq(self):
+        self.read_records = {}
+        if self.fastq_file.endswith(('.fastq.gz', 'fq.gz')):
+            with gzip.open(self.fastq_file, 'rb') as f:
+                for fastq_record in self.chunks(f.read().splitlines(), 4):
+                    fastq_record = [line.decode('utf-8')
+                                    for line in fastq_record]
+                    self.read_records[fastq_record[0]] = [
+                        fastq_record[1], fastq_record[3]]
+        elif self.fastq_file.endswith(('.fastq', '.fq')):
+            with open(self.fastq_file, 'r') as f:
+                for fastq_record in self.chunks(f.read().splitlines(), 4):
+                    self.read_records[fastq_record[0]] = [
+                        fastq_record[1], fastq_record[3]]
+        else:
+            sys.stderr.write('Are you sure this is a fastq file??\n')
+            sys.exit()
+        self.read_records
+
+    def write_fastq(self, fastq_file):
+        with open(fastq_file, 'w') as f:
+            for read_record in self:
+                f.write('{}\n{}\n+\n{}\n'.format(
+                    read_record[0], read_record[1], read_record[2]))
+        pass
+
+    def write_fasta(self, fasta_file):
+        with open(fasta_file, 'w') as f:
+            for read_record in self:
+                f.write('>{}\n{}\n'.format(
+                    read_record[0].strip('@'), read_record[1]))
+            f.close()
